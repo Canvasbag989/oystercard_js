b058e3f4f69b73282797b4b264544fbb868346df
Canvasbag989 <Canvasbag989@icloud.com>
2019-10-02 10:29:23 +0100

    error maximum balance passing

--- jasmine-standalone-3.5.0/spec/oystercard_spec.js
+++ jasmine-standalone-3.5.0/spec/oystercard_spec.js
@@ -1,7 +1,18 @@
+
 describe('oystercard', function() {
+var oystercard;
+    beforeEach(function(){
+        oystercard = new Oystercard()
+    });
     it('can top up', function() {
-        oystercard = new Oystercard();
+        
         oystercard.topup(10);
         expect(oystercard.balance).toEqual(10);
     })
-}) 
+
+    it('has maximum topup limit and raises error', function(){
+        var errorMaximumBalance = 30;
+        oystercard.topup(30);
+        expect(function(){oystercard.topup(1)}).toThrow('Topup limit exceeded');
+    })
+})
--- jasmine-standalone-3.5.0/src/oystercard.js
+++ jasmine-standalone-3.5.0/src/oystercard.js
@@ -1,7 +1,23 @@
 function Oystercard() {
-this.balance = 0;
-}
-
-Oystercard.prototype.topup = function(num) {
-    this.balance += num
-}
+    this.balance = 0;
+    this.maximumBalance = 30;
+    };
+    
+    Oystercard.prototype.topup = function(num) {
+        
+      if (this.balance + num > 30){
+            this.errorMaximumBalance();
+            
+        }
+        else {
+            this.balance += num
+        }
+       
+    
+    };
+    Oystercard.prototype.errorMaximumBalance = function() {
+     if(this.balance == this.maximumBalance){ 
+      throw ('Topup limit exceeded')
+     };
+    };
+    
